6fe33b183f111bbd6e20100dab7413bee2e6d8bb
KenjiMatsumoto <kenjimatsumoto1983@gmail.com>
2019-03-15 17:51:50 +0900

    initial commit

---
+++ src/views/Currency.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>仮想通貨一覧</span>
+                </div>
+                <div>...</div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+	name: "Currency",
+	data () {
+	  return {
+		currencies: []
+	  }
+	},
+	created: async function () {
+	  await this.refresh()
+	},
+	methods: {
+	  refresh: async function () {
+		const res = await axios.get('http://localhost:8080/')
+		this.currencies = res.data.currencies
+		console.info(this.currencies)
+	  }
+	}
+  }
+</script>
+
+<style scoped>
+
+</style>
