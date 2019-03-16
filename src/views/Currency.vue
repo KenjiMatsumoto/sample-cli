4476fd6a32191e4800151a9205d7380a50e50964
kenji matsumoto <kenjimatsumoto1983@gmail.com>
2019-03-16 22:16:50 +0900

    update 登録と削除処理追加

--- src/views/Currency.vue
+++ src/views/Currency.vue
@@ -1,11 +1,36 @@
 <template>
     <el-row>
+				<el-col :span="24">
+						<el-card class="box-card">
+							<el-col :span="8">
+								<el-input v-model="request.name" placeholder="New Name..." clearable>
+								</el-input>
+							</el-col>
+							<el-col :span="8">
+										<el-input v-model="request.symbol" placeholder="New Symbol..." clearable>
+										</el-input>
+							</el-col>
+							<el-col :span="8">
+									<el-button type="success" @click="addCurrency">追加</el-button>
+							</el-col>
+						</el-card>
+				</el-col>
         <el-col :span="24">
             <el-card class="box-card">
                 <div slot="header" class="clearfix">
                     <span>仮想通貨一覧</span>
-                </div>
-                <div>...</div>
+								</div>
+								<el-table :data="currencies" style="width: 100%">
+									<el-table-column prop="id" label="通貨ID" width="300"/>
+									<el-table-column prop="name" label="通貨名" width="300"/>
+									<el-table-column prop="symbol" label="通貨単位" width="300"/>
+									<el-table-column prop="amount" label="数量" width="300"/>
+									<el-table-column prop="operation" label="Ops" width="200" align="left">
+										<template slot-scope="scope">
+											<el-button size="mini" type="danger" @click="deleteCurrency(scope.ro.id)">×</el-button>
+										</template>
+									</el-table-column>
+								</el-table>
             </el-card>
         </el-col>
     </el-row>
@@ -15,21 +40,29 @@
   import axios from 'axios'
   export default {
 	name: "Currency",
-	data () {
-	  return {
-		currencies: []
-	  }
-	},
-	created: async function () {
-	  await this.refresh()
-	},
-	methods: {
-	  refresh: async function () {
-		const res = await axios.get('http://localhost:8080/')
-		this.currencies = res.data.currencies
-		console.info(this.currencies)
-	  }
-	}
+		data () {
+			return {
+				request: {
+					name: undefined,
+					symbol: undefined
+				},
+				currencies: []
+			}
+		},
+		created: async function () {
+			await this.refresh()
+		},
+		methods: {
+			refresh: async function () {
+				const res = await axios.get('http://localhost:8080/')
+				this.currencies = res.data.currencies
+				console.info(this.currencies)
+			},
+			addCurrency: async function() {
+				await axios.post('http://localhost:8080/', this.request)
+				await this.refresh()
+			},
+		}
   }
 </script>
 
