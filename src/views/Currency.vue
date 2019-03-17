cd0503cc092cef1a1616a33b03f20e7af8055ac2
kenji matsumoto <kenjimatsumoto1983@gmail.com>
2019-03-17 20:43:44 +0900

    update スタイルの適用とViewのレイアウト修正

--- src/views/Currency.vue
+++ src/views/Currency.vue
@@ -1,22 +1,37 @@
 <template>
     <el-row>
 				<el-col :span="24">
-						<el-card class="box-card">
-							<el-col :span="8">
-								<el-input v-model="request.name" placeholder="New Name..." clearable>
-								</el-input>
-							</el-col>
-							<el-col :span="8">
-										<el-input v-model="request.symbol" placeholder="New Symbol..." clearable>
-										</el-input>
-							</el-col>
-							<el-col :span="8">
-									<el-button type="success" @click="addCurrency">追加</el-button>
-							</el-col>
+						<el-card class="box-card box-card-wrapper">
+							<div slot="header">
+									<span>新規通貨追加</span>
+							</div>
+							<el-row class="row-wrapper">
+								<el-col :span="12">
+										<span>新規通貨名称</span>
+								</el-col>
+								<el-col :span="12">
+									<el-input v-model="request.name" placeholder="New Name..." clearable>
+									</el-input>
+								</el-col>
+							</el-row>
+							<el-row class="row-wrapper">
+								<el-col :span="12">
+										<span>新規通貨シンボル</span>
+								</el-col>
+								<el-col :span="12">
+											<el-input v-model="request.symbol" placeholder="New Symbol..." clearable>
+											</el-input>
+								</el-col>
+							</el-row>
+							<el-row class="row-wrapper">
+								<el-col :span="24">
+										<el-button type="success" @click="addCurrency">追加</el-button>
+								</el-col>
+							</el-row>
 						</el-card>
 				</el-col>
         <el-col :span="24">
-            <el-card class="box-card">
+            <el-card class="box-card box-card-wrapper">
                 <div slot="header" class="clearfix">
                     <span>仮想通貨一覧</span>
 								</div>
@@ -56,20 +71,31 @@
 			refresh: async function () {
 				const res = await axios.get('http://localhost:8080/')
 				this.currencies = res.data.currencies
-				console.info(this.currencies)
+				this.request.name = undefined
+				this.request.symbol = undefined
 			},
 			addCurrency: async function() {
 				await axios.post('http://localhost:8080/', this.request)
 				await this.refresh()
+        this.$message({
+          showClose: true,
+          message: 'Add Currency Success!',
+          type: 'success'
+        })
 			},
 			deleteCurrency: async function (id) {
 				await axios.delete('http://localhost:8080/' + id)
 				await this.refresh()
+        this.$message({
+          showClose: true,
+          message: 'Delete Currency Success!',
+          type: 'success'
+        })
 			},
 		}
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    @import "../styles/base";
 </style>
